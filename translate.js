1f6cb5ab5cf5ab9d1f8a5550bb4b5847e63dc06f
mazharzaka <mazharmohamed566@gmail.com>
2024-11-16 12:28:44 +0200

    test

---
+++ translate.js
@@ -0,0 +1,22 @@
+
+// import translate from "google-translate-api-x";
+const cors = require("cors");
+const express = require("express");
+const translate = require("google-translate-api-x");
+const app = express();
+app.use(express.json());
+app.use(cors()); 
+app.post("/translate", async (req, res) => {
+    const { text, to } = req.body;
+    try {
+        const result = await translate(text, { to });
+        res.json({ translatedText: result.text });
+    } catch (error) {
+        console.error("Translation error:", error.stack); // Log the full error stack
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
